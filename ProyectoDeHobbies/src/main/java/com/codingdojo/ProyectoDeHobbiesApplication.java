56cfa9d330530b3971f57023c0a2154108c1d0ee
Alfredo08 <alfredosv08@gmail.com>
2022-02-18 10:20:17 -0600

    Primer comit

---
+++ ProyectoDeHobbies/src/main/java/com/codingdojo/ProyectoDeHobbiesApplication.java
@@ -0,0 +1,24 @@
+package com.codingdojo;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+import org.springframework.web.bind.annotation.RequestMapping;
+import org.springframework.web.bind.annotation.RequestMethod;
+import org.springframework.web.bind.annotation.RestController;
+
+@SpringBootApplication
+@RestController
+public class ProyectoDeHobbiesApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(ProyectoDeHobbiesApplication.class, args);
+	}
+	
+	@RequestMapping( value="/", method=RequestMethod.GET )
+	public String despliegaHome() {
+		return "Hola desde Java Spring!";
+	}
+	
+	
+
+}
